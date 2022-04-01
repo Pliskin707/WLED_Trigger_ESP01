128d3aa99a5196ace30dcde45fce4317f74872bd
Pliskin707 <85575336+Pliskin707@users.noreply.github.com>
2022-04-01 09:03:53 +0200

    HTTP Client added
    - added "dprintf()" macro which prints to serial but only for the non-OTA build
    - reads the state of the WLED device and prints it via serial

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <ESP8266WiFi.h>
 #include <ESP8266mDNS.h>
+#include <ESP8266HTTPClient.h>
 #include <ArduinoOTA.h>
 
 /** Wifi authentication **
@@ -15,34 +16,52 @@
  */
 #include "../../../../../../wifiauth2.h"
 
+#ifndef DEBUG_PRINT
+#define dprintf(...) ;
+#else
+#define dprintf(fstr, ...) Serial.printf_P(PSTR(fstr), ##__VA_ARGS__);
+#endif
+
 #define LEDPIN  2
+#define WLEDIP "192.168.0.24"
 const char * deviceName = "WLED_Trigger";
 
 static bool mDNS_init_ok = false;
+WiFiClient client;
+HTTPClient http;
 
 void setup() {
+  #ifndef DEBUG_PRINT
   pinMode(LEDPIN, OUTPUT);
+  #else
+  Serial.begin(115200);
+  #endif
 
-  // Wifi
+  // Wifit
   IPAddress local_IP(192, 168, 0, 40);
   IPAddress gateway(192, 168, 0, 1);
   IPAddress subnet(255, 255, 255, 0);
-  WiFi.config(local_IP, gateway, subnet);
-  wifi_set_sleep_type(NONE_SLEEP_T);
+  IPAddress dns(1, 1, 1, 1);
   WiFi.hostname(deviceName);
   WiFi.mode(WIFI_STA);
+  WiFi.config(local_IP, gateway, subnet, dns);
   WiFi.begin(ssid, password);
+  wifi_set_sleep_type(NONE_SLEEP_T);
 
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
+    #ifndef DEBUG_PRINT
     digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+    #else
+    Serial.println(F("Connecting..."));
+    #endif
   }
 
   // mDNS
   mDNS_init_ok = MDNS.begin(deviceName);
   // if (mDNS_init_ok)
-  //   MDNS.addService("display", "tcp", TCP_PORT);
+  //   MDNS.addService("trigger", "tcp", 8080);
 
   // OTA
   ArduinoOTA.setHostname(deviceName);
@@ -51,7 +70,12 @@ void setup() {
     wifi_set_sleep_type(NONE_SLEEP_T);
   });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    #ifndef DEBUG_PRINT
     digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+    #endif
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    ESP.reset();
   });
   ArduinoOTA.onEnd([]() {
     ESP.reset();
@@ -60,9 +84,14 @@ void setup() {
 }
 
 void loop() {
+  const uint32_t time = millis();
+  static uint32_t next = 0;
+
   // Wifi status
   const bool connected = (WiFi.status() == WL_CONNECTED);
+  #ifndef DEBUG_PRINT
   digitalWrite(LEDPIN, !connected);
+  #endif
 
   // mDNS
   if (mDNS_init_ok)
@@ -70,6 +99,26 @@ void loop() {
 
   // OTA
   ArduinoOTA.handle();
+
+  // program logic
+  if (time >= next)
+  {
+    next = time + 5000;
+    dprintf("Systime: %lu ms\n", time);
+
+    if (connected)
+    {
+      // HTTP
+      http.begin(client, String("http://") + WLEDIP + "/json/state");
+      const int result = http.GET();
+      dprintf("GET result: %d\n", result);
+      if (result == 200)  // == OK
+      {
+        dprintf("Response: \"%s\"\n", http.getString().c_str());
+      }
+      http.end();
+    }
+  }
 
   yield();
 }
